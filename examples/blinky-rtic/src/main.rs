770aff4860e5764a4c19a53d548ffee6d7712059
Henrik Alsér <henrik.alser@me.com>
2021-07-03 00:03:54 +0200

    Generic

--- examples/blinky-rtic/src/main.rs
+++ examples/blinky-rtic/src/main.rs
@@ -1,46 +1,28 @@
 #![no_main]
 #![no_std]
 mod mono;
-
 use defmt_rtt as _;
 use panic_halt as _;
 
-#[rtic::app(device = microbit::hal::pac, peripherals = true, dispatchers = [GPIOTE])]
+#[rtic::app(device = microbit::hal::pac, peripherals = true, dispatchers = [UART0])]
 mod app {
     use super::mono::MonoTimer;
-    use embedded_hal::{digital::v2::OutputPin};
+    use microbit::hal::pac::TIMER0;
     use rtic::time::duration::Seconds;
-    use microbit::hal::{
-        gpio::{p0, Level},
-        timer::{Timer, Periodic},
-        pac::TIMER0
-    };
-
 
     #[monotonic(binds = TIMER0, default = true)]
-    type MyMono = MonoTimer; // 16 MHz
-
-    #[resources]
-    struct Resources {
-        // led: PA5<Output<PushPull>>,
-    }
+    type MyMono = MonoTimer<TIMER0>;
 
     #[init]
-    fn init(mut ctx: init::Context) -> (init::LateResources, init::Monotonics) {
-
-        let mut mono = MonoTimer::new(ctx.device.TIMER0);
-
-        defmt::info!("Hello world!");
-        blink::spawn_after(Seconds(1_u32)).ok();
-        (init::LateResources { }, init::Monotonics(mono))
+    fn init(ctx: init::Context) -> (init::LateResources, init::Monotonics) {
+        let mono = MonoTimer::new(ctx.device.TIMER0);
+        tick::spawn().ok();
+        (init::LateResources {}, init::Monotonics(mono))
     }
 
-    #[task(resources = [])]
-    fn blink(mut ctx: blink::Context) {
-        // ctx.resources.led.lock(|l| l.toggle().ok());
-        defmt::info!("Blink!");
-        blink::spawn_after(Seconds(1_u32)).ok();
+    #[task]
+    fn tick(_: tick::Context) {
+        defmt::info!("Tick");
+        tick::spawn_after(Seconds(1_u32)).ok();
     }
 }
-
-
--- examples/blinky-rtic/src/mono.rs
+++ examples/blinky-rtic/src/mono.rs
@@ -1,9 +1,12 @@
+use microbit::hal::pac::{timer0, TIMER0};
+#[cfg(feature = "v2")]
+use microbit::hal::pac::{TIMER1, TIMER2, TIMER3, TIMER4};
 use rtic_monotonic::{embedded_time, Clock, Fraction, Instant, Monotonic};
 
-pub struct MonoTimer(microbit::hal::pac::TIMER0);
+pub struct MonoTimer<T: Instance32>(T);
 
-impl MonoTimer {
-    pub fn new(timer: microbit::hal::pac::TIMER0) -> Self {
+impl<T: Instance32> MonoTimer<T> {
+    pub fn new(timer: T) -> Self {
         timer.prescaler.write(
             |w| unsafe { w.prescaler().bits(4) }, // 1 MHz
         );
@@ -12,19 +15,19 @@ impl MonoTimer {
     }
 }
 
-impl Clock for MonoTimer {
+impl<T: Instance32> Clock for MonoTimer<T> {
     const SCALING_FACTOR: Fraction = Fraction::new(1, 1_000_000);
     type T = u32;
 
     #[inline(always)]
     fn try_now(&self) -> Result<Instant<Self>, embedded_time::clock::Error> {
-        self.0.tasks_capture[1].write(|w| unsafe { w.bits(1) }); 
+        self.0.tasks_capture[1].write(|w| unsafe { w.bits(1) });
         Ok(Instant::new(self.0.cc[1].read().bits()))
     }
 }
 
-impl Monotonic for MonoTimer {
-    unsafe fn reset(&mut self) { 
+impl<T: Instance32> Monotonic for MonoTimer<T> {
+    unsafe fn reset(&mut self) {
         self.0.intenset.modify(|_, w| w.compare0().set());
         self.0.tasks_clear.write(|w| unsafe { w.bits(1) });
         self.0.tasks_start.write(|w| unsafe { w.bits(1) });
@@ -32,12 +35,23 @@ impl Monotonic for MonoTimer {
 
     fn set_compare(&mut self, instant: &Instant<Self>) {
         #[cfg(feature = "v1")]
-        self.0.cc[0].write(|w| unsafe {w.bits(*instant.duration_since_epoch().integer())});
+        self.0.cc[0].write(|w| unsafe { w.bits(*instant.duration_since_epoch().integer()) });
         #[cfg(feature = "v2")]
-        self.0.cc[0].write(|w| unsafe {w.cc().bits(*instant.duration_since_epoch().integer())}); 
+        self.0.cc[0].write(|w| unsafe { w.cc().bits(*instant.duration_since_epoch().integer()) });
     }
 
     fn clear_compare_flag(&mut self) {
         self.0.events_compare[0].write(|w| w);
     }
 }
+
+pub trait Instance32: core::ops::Deref<Target = timer0::RegisterBlock> {}
+impl Instance32 for TIMER0 {}
+#[cfg(feature = "v2")]
+impl Instance32 for TIMER1 {}
+#[cfg(feature = "v2")]
+impl Instance32 for TIMER2 {}
+#[cfg(feature = "v2")]
+impl Instance32 for TIMER3 {}
+#[cfg(feature = "v2")]
+impl Instance32 for TIMER4 {}
